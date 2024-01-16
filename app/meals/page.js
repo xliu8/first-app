3ca9f801b5e9ab685655b58be07fdc05256515ed
xliu8 <117231787+xliu8@users.noreply.github.com>
2024-01-16 11:17:29 -0600

    new

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,13 @@
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meal-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 export default function MealsPage() {
   return (
     <>
@@ -15,7 +22,9 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
